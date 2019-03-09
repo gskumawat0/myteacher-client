a4e12cc43d2cb949adef7105514f7bc7ed99caf4
gouri shankar kumawat <gskumawat555@gmail.com>
2019-03-09 06:21:55 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuestionPaper from './QuestionPaper.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      errors: ['oops!! it is a error']
+    };
+
+  }
+
+  //add a error
+  addError = (err) => {
+    this.setState({
+      errors: [...this.state.errors, err]
+    });
+
+  }
+
+  removeError = () => {
+    this.setState({
+      errors: []
+    })
+  }
+
+
+
   render() {
+    // debugger
+    let { errors } = this.state;
+    let err = errors && errors.length > 0 && errors.map((error, i) => {
+      return <div className="alert alert-danger alert-dismissible fade show" key={i} role="alert">
+        {error}
+        <button type="button" onClick = {this.removeError} className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+          <span className="navbar-brand mb-0 ml-md-5 h1">Dashboard</span>
+        </nav>
+        <div className='container'>
+          { err  }
+          <QuestionPaper />
+        </div>
       </div>
     );
   }
---
+++ src/QuestionPaper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './QuestionPaper.css';
+
+
+class QuestionPaper extends Component {
+    // constructor(props) {
+            //     super(props);
+            // }
+    render() {
+        return <div> i'm a question paper</div>
+    }
+}
+
+export default QuestionPaper;

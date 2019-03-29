84d407ca2346f7149ef7dad98e0419c59b007de5
gouri shankar kumawat <gskumawat555@gmail.com>
2019-03-29 12:24:44 +0000

    add form to update assigned students

--- src/teachers/QuestionPaper.js
+++ src/teachers/QuestionPaper.js
@@ -8,6 +8,7 @@ class QuestionPaper extends Component {
         super(props);
         this.state = {
             isLoading: true,
+            assignedTo: '',
             questionPaper: {},
         };
     }
@@ -23,6 +24,7 @@ class QuestionPaper extends Component {
                     this.setState({
                         isLoading: false,
                         isSubmitting: false,
+                        assignedTo: data.questionPaper.assignedTo.join(', '),
                         questionPaper: data.questionPaper
                     })
                 }
@@ -41,7 +43,8 @@ class QuestionPaper extends Component {
             [e.target.name]: e.target.value
         })
     }
-
+    
+    
     handleResponseSubmit = (e) => {
         e.preventDefault();
         this.props.removeError();
@@ -71,9 +74,40 @@ class QuestionPaper extends Component {
 
             });
     }
+    
+    handleStudentAssignment = (e)=>{
+        e.preventDefault();
+        this.props.removeError();
+        this.setState({
+            isSubmitting: true
+        })
+
+        apiCall('put', `${process.env.REACT_APP_BASE_URL}/api/questionpapers/${this.props.match.params.questionPaperId}`, {assignedTo: this.state.assignedTo })
+            .then(data => {
+                if (!data.success) {
+                    throw Error(data.message);
+                }
+                else {
+                    this.setState({
+                        isLoading: false,
+                        isSubmitting: false,
+                    })
+                    // this.props.addSuccess(`successfully submitted your responses. you'll receive performance sheet shortly`);
+                    this.props.history.push('/teachers/');
+                }
+            })
+            .catch(err => {
+                this.setState({
+                    isSubmitting: false
+                })
+                this.props.addError(err.message || 'something went wrong. please try again later.')
+
+            });
+    }
+
 
     render() {
-        let { questionPaper, isLoading } = this.state;
+        let { questionPaper, isLoading, assignedTo } = this.state;
         let questionList = !isLoading ? questionPaper.questions.map((question, i) => {
             return (
                 <li key={question._id || i}>
@@ -87,6 +121,16 @@ class QuestionPaper extends Component {
                 <br/>
                 <p className='d-inline mx-5' >total marks: {questionPaper.totalMarks} </p>
                 <p className='float-right d-inline mx-5' >total questions: {questionPaper.totalQuestions}</p>
+                <p className=' mx-5'>Assigned To: {assignedTo}</p>
+                <button className="btn btn-primary mx-5" type="button" data-toggle="collapse" data-target="#collapseForm" aria-expanded="false" aria-controls="collapseForm"> Add students</button>
+                <form className='mx-5 mt-2 collapse' id='collapseForm' onSubmit={this.handleStudentAssignment} method='post'>
+                    <div className='from-group'>
+                        <label htmlFor='studentEmail'>Students Email: </label>  
+                        <input type='text' name='assignedTo' onChange={this.handleChange} value={assignedTo} className='form-control' id='studentEmail'/>
+                        <small>enter the email of the students with comma seperated to assign this question paper. leave empty if wanty to assign to all students of class.</small>
+                    </div>
+                    <button className='btn btn-warning'> Submit</button>
+                </form>
                 <hr className='mb-2'/>
                 <ol>
                     <h1 className='text-center my-3'>Questions:</h1>
